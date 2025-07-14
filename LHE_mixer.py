6ddfdb68ec041e53fed39ea4cc33ec79cceab651
Chi Wang <chiw@lxplus9121.cern.ch>
2025-07-14 12:37:43 +0200

    :tada: First commit!
    :construction: Need an object-oriented version of the functions.

---
+++ LHE_mixer.py
@@ -0,0 +1,12 @@
+"""
+
+
+"""
+from pylhe import *
+import pylhe
+import os
+import numpy as np
+import awkward as ak
+from typing_extensions import List, Dict, Union, Optional, Tuple
+
+# To implement later
